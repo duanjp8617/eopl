8aa6d1685419766963ac824666389cf6e316132c
duanjp8617 <duanjp8617@gmail.com>
2018-06-30 11:38:34 +0800

    Convert PROC to original LET

--- ch3/PROC/syntax.mli
+++ ch3/PROC/syntax.mli
@@ -10,16 +10,7 @@ and expression =
   | IsZeroExp of expression * Ploc.t
   | IfExp of expression * expression * expression * Ploc.t
   | VarExp of string * Ploc.t
-  | BindExp of string * expression * Ploc.t
-  | LetExp of (expression list) * expression * Ploc.t
-  | MinusExp of expression * Ploc.t
-  | EmptyExp of Ploc.t
-  | ConsExp of expression * expression * Ploc.t
-  | CarExp of expression * Ploc.t
-  | CdrExp of expression * Ploc.t
-  | NullExp of expression * Ploc.t
-  | PrintExp of expression * Ploc.t
-  | UnpackExp of (string list) * expression * expression * Ploc.t
-             
+  | LetExp of string * expression * expression * Ploc.t
+
 val parse : char Stream.t -> program
-(* val string_of_program : program -> string *)
+val string_of_program : program -> string
