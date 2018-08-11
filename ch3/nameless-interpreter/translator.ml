979ad257ff9ef51b2c5396590250d44fa7756752
duanjp8617 <duanjp8617@gmail.com>
2018-08-11 17:14:14 +0800

    mid way through 3.43 and 3.44

--- ch3/nameless-interpreter/translator.ml
+++ ch3/nameless-interpreter/translator.ml
@@ -1,21 +1,5 @@
 open Syntax
 
-type environment = string list
-
-exception MissInEnv of string
-                 
-let empty_env () = []
-
-let extend_env variable env = variable :: env
-
-let rec apply_env variable env =
-  match env with
-  | [] -> raise (MissInEnv variable)
-  | x :: ls ->
-     if variable = x
-     then 0
-     else 1 + (apply_env variable ls)
-
 type nl_expression =
   | NlConstExp of int * Ploc.t
   | NlDiffExp of nl_expression * nl_expression * Ploc.t
@@ -25,6 +9,33 @@ type nl_expression =
   | NlLetExp of nl_expression * nl_expression * Ploc.t
   | NlProcExp of nl_expression * (int list) *  Ploc.t
   | NlApplyExp of nl_expression * (nl_expression list) * Ploc.t
+
+
+type env_var =
+  | VarName of string
+  | ProcExp of string * nl_expression * (int list)
+
+and environment = env_var list
+                 
+let empty_env () = []
+
+let extend_env variable env = variable :: env
+
+exception MissInEnv of string
+
+let get_var_name var =
+  match var with
+  | VarName str -> str
+  | ProcExp (str, _, _) -> str
+                     
+let rec apply_env variable env =
+  match env with
+  | [] -> raise (MissInEnv variable)
+  | x :: ls ->
+     if variable = get_var_name x
+     then 0
+     else 1 + (apply_env variable ls)
+
    
 let retrieve_new_env env pos_list =
   let rec do_retrieve_one env pos =
@@ -34,7 +45,9 @@ let retrieve_new_env env pos_list =
        then do_retrieve_one tl (pos-1)
        else hd
     | [] -> raise (MissInEnv "retrieve_new_env fail")
-  in List.map (fun pos -> do_retrieve_one env pos) pos_list
+  in
+  let env_len_minus_one = (List.length env - 1) in 
+  List.map (fun pos -> do_retrieve_one env (env_len_minus_one - pos)) pos_list
 
 let probe_pos exp env =
   let size = List.length env in
@@ -50,13 +63,13 @@ let probe_pos exp env =
        let pos = apply_env str env in
        (if pos > (env_len-(size+1))
         then
-          [pos-(env_len-size)]
+          [(env_len - 1) - pos]
         else
           [])
     | LetExp (str, exp1, exp2, _) ->
-       (do_probe_pos exp1 env) @ (do_probe_pos exp2 (extend_env str env))
+       (do_probe_pos exp1 env) @ (do_probe_pos exp2 (extend_env (VarName str) env))
     | ProcExp (str_list, exp, _) ->
-       (do_probe_pos exp (List.append str_list env))
+       (do_probe_pos exp (List.append (List.map (fun str -> VarName str) str_list) env))
     | RecProcDef _ -> raise (MissInEnv "probe_pos error")
     | ApplyExp (exp1, exp_ls, loc) ->
        List.fold_left (fun l exp -> l @ (do_probe_pos exp env)) (do_probe_pos exp1 env) exp_ls
@@ -87,13 +100,20 @@ let rec translate_of exp env =
   | VarExp (str, loc) -> 
      NlVarExp ((apply_env str env), loc)
   | LetExp (str, exp1, exp2, loc) ->
-     NlLetExp ((translate_of exp1 env), (translate_of exp2 (extend_env str env)), loc)
+     match exp1 with
+     | ProcExp (str_list, proc_body, loc) ->
+        let pos_list = probe_pos (ProcExp (str_list, proc_body, loc)) env in
+        translate_of exp2
+          (extend_env
+             (ProcExp (str,
+                       (translate_of proc_body
+                          (List.append (List.map (fun str -> VarName str) str_list) env)),
+                       pos_list))
+             env)
+     | _ ->
+        NlLetExp ((translate_of exp1 env), (translate_of exp2 (extend_env (VarName str) env)), loc)
   | ProcExp (str_list, exp, loc) ->
-     let pos_list = probe_pos (ProcExp (str_list, exp, loc)) env in 
-     NlProcExp (
-         (translate_of exp (List.append str_list (retrieve_new_env env pos_list))),
-         pos_list,
-         loc)
+     raise (MissInEnv "translate can't translate procedure expression directly") 
   | RecProcDef _ -> raise (MissInEnv "Invalid translation of RecProcDef")
   | ApplyExp (exp1, exp_ls, loc) ->
      NlApplyExp ((translate_of exp1 env), List.map (fun exp -> translate_of exp env) exp_ls, loc)     
