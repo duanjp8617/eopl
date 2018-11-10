0399a4d1195b1c4c461c44986582357b18c12f3b
djp <duanjp8617@gmail.com>
2018-11-10 13:59:58 +0800

    Working on exception

---
+++ ch5/EXCEPTION/syntax.mli
@@ -0,0 +1,21 @@
+type program =
+  | AProgram of top_level list
+
+and top_level =
+  | ExpTop of expression
+
+and expression =
+  | ConstExp of int * Ploc.t
+  | DiffExp of expression * expression * Ploc.t
+  | IsZeroExp of expression * Ploc.t
+  | IfExp of expression * expression * expression * Ploc.t
+  | VarExp of int * Ploc.t
+  | LetExp of expression * expression * Ploc.t
+  | ProcExp of expression * Ploc.t
+  | ApplyExp of expression * expression * Ploc.t
+  | LetRecExp of (expression list) * expression * Ploc.t
+  | MultiLetExp of (expression list) * expression * Ploc.t
+
+               
+val parse : char Stream.t -> program
+
